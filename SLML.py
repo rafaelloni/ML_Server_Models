52bb36aa24f5b612c43939ec23de6e8d80517429
rafael loni <rafaellonim@gmail.com>
2020-02-06 13:53:07 -0300

    update

--- SLML.py
+++ SLML.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+### MENU VARIABLES ###
 super_unsuper = ""
 super_regre_class = ""
 ML_option = ""
@@ -16,7 +17,7 @@ ML_option = ""
 
 
 ########################################
-# Machine Learning Algorithms
+# Machine Learning Algorithms Menu
 st.sidebar.title("Machine Learning Models")
 super_unsuper = st.sidebar.radio("Choose a model", ("Supervised Learning", "Unsupervised Learning"))
 if super_unsuper == "Supervised Learning":
@@ -27,34 +28,38 @@ if super_unsuper == "Supervised Learning":
         pass
 else:
     pass
+########################################
 
 
 
-
-
-
+########################################
+# Title
 st.title("Machine Learning Analysis")
 
-def TakeFile():
+# Function to get csv file
+def GetFile():
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
     if uploaded_file is not None:
         data = pd.read_csv(uploaded_file)
         return data
 
-def TakeFeatures(dataframe, colunas):
+# Function to get features
+def GetFeatures(dataframe, colunas):
     df_X = dataframe[[colunas[0]]]
     for i in range(1,len(colunas)):
         df_X = df_X.join(dataframe[[colunas[i]]])
     return df_X
 
-def TakeTarget(dataframe, colunas):
+# Function to get target
+def GetTarget(dataframe, colunas):
     df_X = dataframe[[colunas[0]]]
     for i in range(1,len(colunas)):
         df_X = df_X.join(dataframe[[colunas[i]]])
     return df_X
 
+# Show CSV head
 try:
-    data_csv = TakeFile()
+    data_csv = GetFile()
     st.write(data_csv.head())
 except:
     st.write("Adicione um dataset.")
@@ -62,22 +67,27 @@ except:
 X_features = st.text_input("Features: ").split()
 y_target = st.text_input("Target: ").split()
 try:
-    data_feature = TakeFeatures(data_csv, X_features)
-    data_target = TakeTarget(data_csv, y_target)
+    data_feature = GetFeatures(data_csv, X_features)
+    data_target = GetTarget(data_csv, y_target)
     st.write("Features", data_feature.head(), "Target",data_target.head())
 except:
     st.write("Não foi possível carregar feature e target.")
 
+# Train Test Split
 lr_ts = st.number_input("Test size: ")
-lr_rs = st.number_input("Random state: ")
+lr_rs = st.number_input("Random state: ", min_value=1, step=1)
 try:
     X_train, X_test, y_train, y_test = train_test_split(data_feature, data_target, test_size=lr_ts, random_state=int(lr_rs))
 except:
     st.write("Não possivel separar os dados")
+########################################
+
+
 
 ########################################
 # LINEAR REGRESSION
 if ML_option == "Linear Regression":
+    # Fit the model and predict X_test. Show some analysis.
     try:
         linReg = LinearRegression()
         linReg.fit(X_train, y_train)
@@ -89,12 +99,14 @@ if ML_option == "Linear Regression":
     except:
         st.write("Preencha todos os parâmetros")
 
+    # Show features coefficient
     try:
         coeff_df = pd.DataFrame(linReg.coef_,index=["Coefficient"] ,columns=[data_feature.columns])
         st.write(coeff_df)
     except:
         pass
-
+    
+    # Scatter Plot
     try:
         plt.scatter(y_test,pred)
         plt.xlabel("Real")
@@ -102,6 +114,8 @@ if ML_option == "Linear Regression":
         st.pyplot()
     except:
         pass
+
+    # Distribuition Plot
     try:
         ibins = st.number_input("bins: ",min_value=1,step=1)
         sns.distplot((y_test-pred),bins=int(ibins))
